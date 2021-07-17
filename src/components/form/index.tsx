3eea2c19da0bdd202de642d7e1304adcff29ee5e
Thiago Queiroz Pirolla <thiago.pirolla@gmail.com>
2021-07-16 22:49:13 -0300

    Adding page file and component form

---
+++ src/components/form/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import * as S from './style'
+
+function form() {
+    return(
+        <>
+        </>
+    )
+}
---
+++ src/pages/landingPage/index.tsx
@@ -0,0 +1,12 @@
+
+import { useState } from 'react'
+import * as S from './style'
+
+function LandingPage() {
+    
+    return (
+        <h1>Eai caralho</h1>
+    )
+}
+
+export default LandingPage()

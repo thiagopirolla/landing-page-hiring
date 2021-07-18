404327c5a01521b33c7a5de11534ff6eab1c47a0
Thiago Queiroz Pirolla <thiago.pirolla@gmail.com>
2021-07-18 20:09:03 -0300

    fix: solved bug image that get very spaced in smaller layouts

---
+++ src/components/secondSection/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import * as S from "./style";
+import { ReactComponent as Imagem2 } from './../../assets/imagem2.svg';
+import Form from "../form";
+
+
+
+export default function SecondSection() {
+  return (
+    <S.Container>
+      <S.Title>Promoções de Black Friday!!!!</S.Title>
+      <S.description>
+        Nessa Black Friday, não fique de fora das promoções mais em conta desse país!!
+      </S.description>
+
+      <Form/>
+      <S.Image><Imagem2 style={{maxWidth:"600px", width:"100%", height:"100%"}}/></S.Image>
+      
+    </S.Container>
+  );
+}
